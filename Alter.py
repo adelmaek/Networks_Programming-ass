00b1f195bd9e1e26a871afec44f3eb7e5fc89782
SSLotfi <salma.s.lotfi@gmail.com>
2018-11-20 00:17:47 +0200

    alter function #2

---
+++ Alter.py
@@ -0,0 +1,10 @@
+def Alter(correct_message , bit_position):
+    num_bits = correct_message.bit_length()
+    error_bit = 1 << (num_bits - bit_position)
+    altered_msg = correct_msg ^ error_bit
+
+    return altered_msg
+
+
+correct_msg = 50  #110010
+print(bin(Alter(correct_msg , 3)))
