99e8dca78dce2e99449c8ef101e2937cebad34c0
adel aek <adelmahmoud1997@gmail.com>
2018-11-21 12:22:27 +0200

    last update by adel #3

--- CRC error detection/Alter.py
+++ CRC error detection/Alter.py
@@ -1,12 +1,15 @@
 def Alter(correct_message , bit_position):
+    correct_message = int(correct_message)
+    print(correct_message)
+    bit_position = int(bit_position)
     num_bits = correct_message.bit_length()
-    if (bit_position < correct_message.bit_length()) :
+    if bit_position <  correct_message.bit_length():
         error_bit = 1 << (num_bits - bit_position)
-        altered_msg = correct_message ^ error_bit
+        altered_msg =  correct_message ^ error_bit
     else :
-        altered_msg = correct_message
-
-    return altered_msg
+        altered_msg =  correct_message
+    print(altered_msg)
+    return str(altered_msg)
 
 
 #correct_msg = 50  #110010
--- CRC error detection/CRC.py
+++ CRC error detection/CRC.py
@@ -12,13 +12,14 @@ def main():
         break
     message = ifhand.readline()
     genFunc = ifhand.readline()
+    print("message: "+ message)
+    print("genFunc: "+genFunc)
     while True:
         print("Select command:\nGenerator only -> 1\nGenerator-verifier -> 2\nGenerator-Verifier-Alter-Verifier->3\n")
         command = input("Enter Command number:")
         if command == '1':
             #print("Generator")
-            print("Transmitted message: ")
-            print(generator(message,genFunc))
+            print("Transmitted message: " +generator(message,genFunc))
         elif command == '2':
             #print("Generator-Verifier")
             transmittedMessage = generator(message,genFunc)
@@ -31,9 +32,14 @@ def main():
             Verifier(transmittedMessage, genFunc)
             bit_position = input("Which bit to alter? ")
             altered_msg = Alter(transmittedMessage,bit_position)
-            Verifier(altered_msg)
+            Verifier(altered_msg,genFunc)
         else:
             print("Choose a valid command number")
+            continue
+        exit  = input("exit?Y/N")
+        if exit == 'y' or exit == 'Y':
+            break
+        else:
             continue
 
 if __name__ == '__main__':
