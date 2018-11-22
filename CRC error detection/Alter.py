a06d4c80864db3d13a834c0885a98dc13ecb9883
SSLotfi <44554606+SSLotfi@users.noreply.github.com>
2018-11-22 10:30:37 +0200

    updates alter function

--- CRC error detection/Alter.py
+++ CRC error detection/Alter.py
@@ -8,8 +8,8 @@ def Alter(correct_message , bit_position):
         altered_msg =  correct_message ^ error_bit
     else :
         altered_msg =  correct_message
-    print('altered message: ' + bin(altered_msg)[2:])
-    return bin(altered_msg)[2:]
+    print('altered message: ' + bin(altered_msg)[2:].zfill(correct_message.bit_length()))
+    return bin(altered_msg)[2:].zfill(correct_message.bit_length()
 
 
 #correct_msg = 50  #110010
