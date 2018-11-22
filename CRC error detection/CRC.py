074b54c7ab0240beb82f87e2751b565dfa42523d
adel aek <adelmahmoud1997@gmail.com>
2018-11-22 12:46:41 +0200

    #3

--- CRC error detection/CRC.py
+++ CRC error detection/CRC.py
@@ -3,35 +3,30 @@ from verifier import Verifier
 from Alter import Alter
 def main():
     while True:
-        try:
-            inputFile = input("input file path: ")
-            ifhand = open(inputFile)
-        except FileNotFoundError:
-            print("File not Found.")
-            continue
-        break
-    message = ifhand.readline()
-    genFunc = ifhand.readline()
-    ## handling wrong inputs-> eg: extra spaces or \n
-    message= message.replace(" ","")
-    message = message.replace("\n",'')
-    genFunc = genFunc.replace(" ","")
-    genFunc = genFunc.replace("\n","")
-
-
-    while True:
-        print("Select command:\nGenerator only -> 1\nGenerator-verifier -> 2\nGenerator-Verifier-Alter-Verifier->3\n")
-        command = input("Enter Command number:")
+        while True:
+            try:
+                inputFile = input("input file path: ")
+                ifhand = open(inputFile)
+            except FileNotFoundError:
+                print("File not Found.")
+                continue
+            break
+        message = ifhand.readline()
+        genFunc = ifhand.readline()
+        ## handling wrong inputs-> eg: extra spaces or \n
+        message = message.replace(" ", "")
+        message = message.replace("\n", '')
+        genFunc = genFunc.replace(" ", "")
+        genFunc = genFunc.replace("\n", "")
+        print("Commands:\n  Generator only -> 1\n  Generator-verifier -> 2\n  Generator-Verifier-Alter-Verifier->3\n")
+        command = input("Enter command number:")
         if command == '1':
-            #print("Generator")
             print("Transmitted message: " +generator(message,genFunc))
         elif command == '2':
-            #print("Generator-Verifier")
             transmittedMessage = generator(message,genFunc)
             print("transmitted message: "+transmittedMessage)
             Verifier(transmittedMessage,genFunc)
         elif command == '3':
-            #print("Generator-Varifier-Alter-Verifier")
             transmittedMessage = generator(message, genFunc)
             print("transmitted message: " + transmittedMessage)
             Verifier(transmittedMessage, genFunc)
@@ -41,10 +36,11 @@ def main():
         else:
             print("Choose a valid command number")
             continue
-        exit  = input("exit?Y/N")
+        exit  = input("exit?Y/N\n")
         if exit == 'y' or exit == 'Y':
             break
         else:
+            print("__________________________\n\n")
             continue
 
 if __name__ == '__main__':
